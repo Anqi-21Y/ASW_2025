d0afa7ec019c7d0ae9356856e4941d16fe140ee5
Anqi <anqiyang1270@alumnes.ilerna.com>
2026-01-19 21:11:24 +0100

    Fase 2 compledo

--- Laboratorio 9/Fase_2/script.js
+++ Laboratorio 9/Fase_2/script.js
@@ -1,6 +1,7 @@
 const body = document.getElementById("main-body");
 
 body.addEventListener("keydown",function(e){
+    
     console.log("Tipo de evento:", e.type);
     console.log("Tecla de pulsada: ", e.key);
 });
